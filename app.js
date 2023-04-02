e7a91d2d5d054875242105a2deea4bc8b9fe13c8
Andrew Santiago <santiago.an@northeastern.edu>
2023-04-02 19:32:54 -0400

    a9 Mongo fa22
    a9 Mongo fa22

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
 const app = express()
 app.use(cors())
 app.use(express.json());
+
 TuitsController(app);
 HelloController(app)
 UserController(app)
