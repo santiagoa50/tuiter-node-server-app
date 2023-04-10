b90f04ce7f51b60823a85decbc86112e3f2c86cb
Andrew Santiago <santiago.an@northeastern.edu>
2023-04-09 20:43:44 -0400

    a9 Mongo sp23
    a9 Mongo sp23

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ import UserController
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
- mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+ //mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
 
-/*
+
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb+srv://santiagoan:hellomeow12@cluster0.fba779y.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(CONNECTION_STRING);
-*/
+
 const app = express()
 app.use(cors())
 app.use(express.json());
